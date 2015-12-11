e9540d9a3053f2ddca53034bca164f3a66b405b6
Loi Wu <superloiwu@yahoo.com>
2015-12-11 16:29:20 +0800

    branch-hybrid

---
+++ MyPages/MyPages/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyPages
+//
+//  Created by Loi Wu on 12/11/15.
+//  Copyright © 2015 Loi Wu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
